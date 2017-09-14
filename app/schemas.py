2c4b3677166f3ba1fb89a7b4b58e08fb7016f8fa
Rohan Roy <b3you0870@gmail.com>
2017-09-14 18:57:03 +0530

    Schema was modified

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from marshmallow import validates, ValidationError
+from marshmallow import validates, ValidationError, fields, pre_load, pre_dump
 
 from app import ma
 
@@ -22,13 +22,15 @@ class VendorSchema(ma.Schema):
     class Meta:
         fields = ('id', 'name', 'status', '_links')
 
-    status = ma.Nested(StatusSchema)
+    status = fields.Nested(StatusSchema, only=('name',), required=False)
 
     @validates('name')
     def validate_name(self, value):
         if value is None or not value:
             raise ValidationError('Vendor name can\'t be blank')
 
+    # @pre_load
+    # def
     _links = ma.Hyperlinks({
         'self': ma.URLFor('vendor_route', id='<id>'),
         'collection': ma.URLFor('vendor_route')
@@ -41,8 +43,8 @@ class BusinessSchema(ma.Schema):
         fields = ('id', 'name', 'status', 'vendors', 'created_at', 'modified_at', '_links')
         ordered = True
 
-    status = ma.Nested(StatusSchema)
-    vendors = ma.Nested(VendorSchema, many=True)
+    status = ma.Nested(StatusSchema, only=('name',))
+    vendors = ma.Nested(VendorSchema, many=True, only=('id', 'name', 'status'))
 
     @validates('name')
     def validate_name(self, value):
@@ -58,17 +60,15 @@ class BusinessSchema(ma.Schema):
 
 class PartnerSchema(ma.Schema):
     class Meta:
-        fields = ('name', 'vendor')
+        fields = ('name',)
+
+    vendor = fields.Nested(VendorSchema, required=False, only=('name',))
+    status = fields.Nested(StatusSchema, required=False, only=('name',))
 
     @validates('name')
     def validate_name(self, value):
         if value is None or not value:
             raise ValidationError('Business name can\'t be blank')
-
-    @validates('vendor')
-    def validate_vendor(self, value):
-        if value is None or not value:
-            raise ValidationError('Vendor name can\'t be blank')
 
 
 business_schema = BusinessSchema()
